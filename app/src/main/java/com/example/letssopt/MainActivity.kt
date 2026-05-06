a4e00d0551a220a4ddeadb7ec56c8fd478086804
yerim0829 <123yellly@gmail.com>
2026-05-06 19:53:09 +0900

    week1 수정본3

--- app/src/main/java/com/example/letssopt/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/MainActivity.kt
@@ -1,29 +1,24 @@
 package com.example.letssopt
-import android.R.attr.text
+
 import android.content.Intent
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -34,226 +29,185 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.app.Activity
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.example.letssopt.ui.theme.LETSSOPTTheme
 
 class MainActivity : ComponentActivity() {
 
-    var savedId by mutableStateOf("")
-    var savedPw by  mutableStateOf("")
-
-    val signUpLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data = result.data
-                savedId = data?.getStringExtra("id") ?: ""
-                savedPw = data?.getStringExtra("pw") ?: ""
-            }
-        }
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             LETSSOPTTheme {
-                MyScreen()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
 
+                    Login( modifier= Modifier.padding(innerPadding))
+                }
             }
         }
     }
 }
 
-
+//로그인 화면
 @Composable
-fun MyScreen( ) {
-
-    var id by remember {mutableStateOf("")}
-    var pw by remember {mutableStateOf(value= "")}
-
-
-    val context=  LocalContext.current
+fun Login(
+    modifier: Modifier = Modifier,
+    onSignUpClick : () -> Unit= {}
+) {
+    var email by remember {mutableStateOf("")}
+    var password by remember {mutableStateOf("")}
+    val context = LocalContext.current
 
     Column(
         modifier= Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(Color(0xFF111111))
+            .padding(20.dp) ,
+
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Text(
-            text= "watcha",
-            color= Color(0xFF800020),
-            modifier= Modifier
-                .fillMaxWidth()
-                .padding (top= 70.dp)
-                .padding( bottom = 15.dp),
-
-            textAlign= TextAlign.Center ,
-            fontSize=50.sp,
-            fontWeight= FontWeight.Bold
-
-
+            text = "watcha" ,
+            color= Color (0xFFE8003C),
+            fontSize=36.sp,
+            fontWeight= FontWeight.Bold ,
+            modifier= Modifier.padding (top=60.dp)
         )
+
+        Spacer ( modifier= Modifier.height(26.dp))
+
 
         Text (
-            text =" 이메일로 로그인",
-            color= Color.White,
-            fontSize= 22.sp,
-            fontWeight= FontWeight.Bold ,
-            modifier=Modifier
-                .padding( 15.dp)
+            text= "이메일로 로그인",
+            color=Color(0xFFFFFFFF) ,
+            fontSize= 20.sp,
+            fontWeight= FontWeight.Bold,
+            modifier= Modifier.align(Alignment.Start)
 
         )
 
-        Spacer (
-            modifier= Modifier.height(30.dp)
+        Spacer (modifier= Modifier.height(36.dp))
+
+        Text (
+            text= "이메일",
+            color=Color.Gray,
+            fontSize= 14.sp,
+            modifier=Modifier.align(Alignment.Start)
+
         )
 
-        Column {
+        Spacer (modifier= Modifier.height(3.dp))
 
-            Text(
-                text= "이메일",
-                fontSize=16.sp ,
+        TextField(
+            value= email,
+            onValueChange = { email = it},
+            modifier= Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+            placeholder = {Text ("이메일 주소를 입력하세요")},
+            shape= RoundedCornerShape(8.dp) ,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF2A2A2A),
+                unfocusedContainerColor = Color(0xFF2A2A2A),
+
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedTextColor= Color.White,
+                unfocusedTextColor= Color.White,
+                focusedPlaceholderColor= Color.Gray,
+                unfocusedPlaceholderColor = Color.Gray
+
+            )
+        )
+
+        Spacer (modifier= Modifier.height(18.dp))
+
+        Text (
+            text= "비밀번호",
+            color=Color.Gray,
+            fontSize= 14.sp,
+            modifier=Modifier.align(Alignment.Start)
+
+        )
+
+        Spacer (modifier= Modifier.height(3.dp))
+
+        TextField(
+            value= password ,
+            onValueChange = { password = it},
+            modifier= Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+            placeholder = {Text ("비밀번호를 입력하세요")},
+            shape= RoundedCornerShape(8.dp) ,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF2A2A2A),
+                unfocusedContainerColor = Color(0xFF2A2A2A),
+
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedTextColor= Color.White,
+                unfocusedTextColor= Color.White,
+                focusedPlaceholderColor= Color.Gray,
+                unfocusedPlaceholderColor = Color.Gray
+            )
+        )
+
+
+        Spacer(modifier= Modifier.weight(1f))
+
+
+        Row  {
+
+            Text (
+                text= "아직 계정이 없으신가요? 회원가입",
                 color= Color.Gray,
-                modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .padding(bottom =8.dp)
+                fontSize=14.sp,
+                modifier= Modifier.clickable {
+                    val intent = Intent(context, SignUpActivity::class.java)
+                    context.startActivity(intent)
+                }
             )
-
-            TextField(
-                value= id,
-                onValueChange = { id = it},
-                placeholder = {
-                    Text ("이메일 주소를 입력하세요")
-                } ,
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFF1E1E1E),
-                    unfocusedContainerColor = Color(0xFF1E1E1E),
-                    focusedPlaceholderColor = Color.Gray,
-                    unfocusedPlaceholderColor = Color.Gray ,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White,
-                ),
-                shape= RoundedCornerShape(10.dp),
-
-                modifier=Modifier.fillMaxWidth()
-                    .padding(horizontal= 16.dp)
-            )
-
-            Spacer (modifier=Modifier.height (15.dp))
-
-            Text(
-                text ="비밀번호",
-                fontSize=16.sp,
-                color= Color.Gray,
-                modifier= Modifier.fillMaxWidth()
-                    .padding(horizontal= 20.dp)
-                    .padding(bottom= 8.dp)
-            )
-
-            TextField(
-                value=pw,
-                onValueChange = {pw = it},
-                visualTransformation = PasswordVisualTransformation(),
-                placeholder = {
-                    Text ("비밀번호를 입력하세요")
-                },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFF1E1E1E),
-                    unfocusedContainerColor = Color(0xFF1E1E1E),
-                    focusedPlaceholderColor = Color.Gray,
-                    unfocusedPlaceholderColor = Color.Gray ,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White,
-                ),
-                shape= RoundedCornerShape(10.dp),
-                modifier=Modifier.fillMaxWidth()
-                    .padding (horizontal = 16.dp)
-            )
-
         }
 
-        Spacer (
-            modifier=Modifier.weight(1f)
-        )
-
-        TextButton (
-            onClick= {
-                val intent = Intent(context, SignUpActivity::class.java)
-                (context as MainActivity).signUpLauncher.launch(intent)
-            },
-            modifier=Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(0.dp)
-        ) {
-
-            Text ("아직 계정이 없으신가요? 회원가입",
-                fontSize= 12.sp,
-                color= Color.Gray,
-            )
-        }
+        Spacer (modifier= Modifier.height(20.dp))
 
         Button (
-            onClick= {
-
-                if ( id == (context as MainActivity).savedId &&
-                    pw == (context as MainActivity).savedPw &&
-                    (context as MainActivity).savedId.isNotEmpty()) {
-
-                    Toast.makeText (context , "로그인에 성공했습니다",
-                        Toast.LENGTH_SHORT).show() }
-                else {
-
-
-                    Toast.makeText(
-                        context,
-                        "아이디 또는 비밀번호가 다릅니다",
-                        Toast.LENGTH_SHORT
-                    ).show()
-
-
-                }
-            },
-            shape= RoundedCornerShape(8.dp),
-            colors= ButtonDefaults.buttonColors (
-                containerColor= Color(0xFF9B1B30)
-            ),
-            modifier=Modifier
+            onClick= {},
+            modifier= Modifier
                 .fillMaxWidth()
-                .height (75.dp)
-                .padding (bottom= 24.dp)
-                .padding (horizontal = 16.dp)
+                .height(52.dp),
+            colors= ButtonDefaults.buttonColors(
+                containerColor= Color(0xFFE8003C)
+            ),
+            shape= RoundedCornerShape(8.dp))
 
-        ) {
-
-            Text ("로그인",
-                fontSize = 17.sp
-
+        {
+            Text(
+                text= "로그인",
+                fontSize=16.sp,
+                fontWeight= FontWeight.Bold
             )
+
         }
 
 
     }
-
 }
-
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewMyScreen() {
-    MyScreen()
+fun GreetingPreview() {
+    LETSSOPTTheme {
+        Login()
+    }
 }
--- app/src/main/java/com/example/letssopt/SignUpActivity.kt
+++ app/src/main/java/com/example/letssopt/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.letssopt
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -23,16 +24,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.app.Activity
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,31 +62,31 @@ fun SignUpScreen() {
 
         Text(
             text = "watcha",
-            color = Color(0xFF800020),
+            color = Color(0xFFE8003C),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 70.dp)
-                .padding(bottom = 15.dp),
+                .padding(top = 60.dp),
 
             textAlign = TextAlign.Center,
-            fontSize = 50.sp,
+            fontSize = 36.sp,
             fontWeight = FontWeight.Bold
 
 
         )
 
+        Spacer (modifier = Modifier.height(26.dp))
+
         Text(
             text = "회원가입",
             color = Color.White,
-            fontSize = 22.sp,
+            fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .padding(15.dp)
-
+                .padding(start= 20.dp)
         )
 
         Spacer(
-            modifier = Modifier.height(30.dp)
+            modifier = Modifier.height(36.dp)
         )
 
         Column {
@@ -94,9 +95,9 @@ fun SignUpScreen() {
                 text = "이메일",
                 fontSize = 14.sp,
                 color = Color.Gray,
-                modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .padding(bottom = 8.dp)
+                modifier=Modifier.align(Alignment.Start)
+                    .fillMaxWidth()
+                    .padding(start= 20.dp)
             )
 
             TextField(
@@ -115,21 +116,21 @@ fun SignUpScreen() {
                     focusedTextColor = Color.White,
                     unfocusedTextColor = Color.White,
                 ),
-                shape = RoundedCornerShape(10.dp),
+                shape = RoundedCornerShape(8.dp),
 
                 modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 20.dp)
             )
 
-            Spacer(modifier = Modifier.height(15.dp))
+            Spacer(modifier = Modifier.height(18.dp))
 
             Text(
                 text = "비밀번호",
                 fontSize = 14.sp,
                 color = Color.Gray,
                 modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .padding(bottom = 8.dp)
+                    .padding(start= 20.dp)
+                    .padding(bottom = 3.dp)
             )
 
             TextField(
@@ -149,21 +150,22 @@ fun SignUpScreen() {
                     focusedTextColor = Color.White,
                     unfocusedTextColor = Color.White,
                 ),
-                shape = RoundedCornerShape(10.dp),
+                shape = RoundedCornerShape(8.dp),
                 modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 20.dp)
             )
 
-            Spacer(modifier = Modifier.height(15.dp))
+            Spacer(modifier = Modifier.height(18.dp))
 
             Text(
                 text = "비밀번호 확인",
                 fontSize = 14.sp,
                 color = Color.Gray,
                 modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .padding(bottom = 8.dp)
+                    .padding(start = 20.dp)
+                    .padding(bottom = 3.dp)
             )
+            Spacer(modifier = Modifier.height(3.dp))
 
             TextField(
                 value = pwCheck,
@@ -182,9 +184,9 @@ fun SignUpScreen() {
                     focusedTextColor = Color.White,
                     unfocusedTextColor = Color.White,
                 ),
-                shape = RoundedCornerShape(10.dp),
+                shape = RoundedCornerShape(8.dp),
                 modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .padding(horizontal = 20.dp)
             )
 
         }
@@ -223,22 +225,23 @@ fun SignUpScreen() {
             enabled = isSignUpEnabled,
             shape = RoundedCornerShape(8.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF9B1B30),
+                containerColor = Color(0xFFE8003C),
                 contentColor = Color.White,
-                disabledContainerColor = Color(0xFF5A5A5A),
-                disabledContentColor = Color(0xFFCCCCCC)
+                disabledContainerColor = Color(0xFFE8003C) ,
+                disabledContentColor= Color.White,
+
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .height(75.dp)
-                .padding(bottom = 24.dp)
-                .padding(horizontal = 16.dp)
+                .padding(bottom = 26.dp)
+                .padding(horizontal = 20.dp)
+                .height(52.dp)
 
         ) {
 
             Text(
                 "회원가입",
-                fontSize = 17.sp
+                fontSize = 16.sp
 
             )
         }
